0c9f06bdb51319bcedb107c2d5541098bc2c26ac
lcw <91087872+LilyIsCurious@users.noreply.github.com>
2022-01-30 14:57:23 -0500

    Update TrashApplication.java

--- app/src/main/java/com/example/myrecycle/TrashApplication.java
+++ app/src/main/java/com/example/myrecycle/TrashApplication.java
@@ -12,10 +12,10 @@ public class TrashApplication extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        //初始化网络框架
+        //初始化网络框架 Initialize network framework
         NetworkApi.init(new NetworkRequiredInfo(this));
 
-        //配置讯飞语音SDK
+        //配置讯飞语音SDK Configure iFLYTEK voice SDK
         SpeechUtility.createUtility(this, SpeechConstant.APPID + "=c997c34f");
     }
 }
