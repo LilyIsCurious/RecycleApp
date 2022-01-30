7a51747900aee015927892f07b76a78b08fc4aa0
lcw <91087872+LilyIsCurious@users.noreply.github.com>
2022-01-30 14:54:50 -0500

    Update VoiceInputActivity.java

--- app/src/main/java/com/example/myrecycle/ui/VoiceInputActivity.java
+++ app/src/main/java/com/example/myrecycle/ui/VoiceInputActivity.java
@@ -25,12 +25,13 @@ import java.util.List;
 
 /**
  * 语音输入物品进行垃圾分类
+ * Voice input items for garbage classification
  */
 public class VoiceInputActivity extends MvpActivity<VoiceContract.VoicePresenter> implements VoiceContract.VoiceView {
 
     private MaterialToolbar toolbar;//工具栏
     private static final String TAG = "VoiceInputActivity";
-    private RxPermissions rxPermissions;//异步权限请求框架
+    private RxPermissions rxPermissions;//异步权限请求框架 Asynchronous permission request framework
     private List<TrashResponse.NewslistBean> newslistBeanList = new ArrayList<>();//数据列表
     private SearchGoodsAdapter searchGoodsAdapter;//结果列表适配器
     private RecyclerView rvResult;//结果列表
@@ -43,11 +44,11 @@ public class VoiceInputActivity extends MvpActivity<VoiceContract.VoicePresenter
             if (goodsName.isEmpty()) {
                 return;
             }
-            //判断字符串是否包含句号
+            //判断字符串是否包含句号 Judge whether the string contains a period
             if (goodsName.contains("。")) {
                 return;
             }
-            //请求接口搜索物品的垃圾分类
+            //请求接口搜索物品的垃圾分类 Request interface to search for garbage classification of articles
             showMsg("正在搜索物品：" + goodsName);
             mPresenter.searchGoods(goodsName);
         });
@@ -69,17 +70,17 @@ public class VoiceInputActivity extends MvpActivity<VoiceContract.VoicePresenter
         //隐藏加载弹窗
         hideLoadingDialog();
         if (response.getCode() == Constant.SUCCESS_CODE) {
-            //请求成功  进行数据的渲染
+            //请求成功  进行数据的渲染 Request to render data successfully
             if (response.getNewslist() != null && response.getNewslist().size() > 0) {
                 newslistBeanList.clear();
                 newslistBeanList.addAll(response.getNewslist());
-                //刷新适配器
+                //刷新适配器 Refresh adapter
                 searchGoodsAdapter.notifyDataSetChanged();
             } else {
                 showMsg("触及到了知识盲区");
             }
         } else {
-            //显示请求接口失败的原因
+            //显示请求接口失败的原因 Displays the reason why the request interface failed
             showMsg(response.getMsg());
         }
     }
@@ -105,25 +106,25 @@ public class VoiceInputActivity extends MvpActivity<VoiceContract.VoicePresenter
     private void initView() {
         toolbar = findViewById(R.id.toolbar);
         rvResult = findViewById(R.id.rv_result);
-        //设置页面状态栏
+        //设置页面状态栏 Set page status bar
         setStatubar(this, R.color.white, true);
-        //设置返回工具栏图标点击事件
+        //设置返回工具栏图标点击事件 Set return toolbar icon click event
         back(toolbar, false);
-        //实例化
+        //实例化 instantiation
         rxPermissions = new RxPermissions(this);
 
-        //初始化语音
+        //初始化语音 Initialize voice
         SpeechUtil.init(context);
 
-        //配置适配器  设置布局和数据源
+        //配置适配器  设置布局和数据源 Configure adapter settings layout and data source
         searchGoodsAdapter = new SearchGoodsAdapter(R.layout.item_search_rv, newslistBeanList);
-        //设置列表的布局管理器
+        //设置列表的布局管理器 Set the layout manager for the list
         rvResult.setLayoutManager(new LinearLayoutManager(this));
-        //列表item点击事件
+        //列表item点击事件 List item click event
         searchGoodsAdapter.setOnItemChildClickListener((adapter, view, position) -> {
             showMsg("点击了" + newslistBeanList.get(position).getName());
         });
-        //设置列表适配器
+        //设置列表适配器 Set list adapter
         rvResult.setAdapter(searchGoodsAdapter);
     }
 
@@ -137,7 +138,7 @@ public class VoiceInputActivity extends MvpActivity<VoiceContract.VoicePresenter
         showMsg("语音");
     }
     /**
-     * 悬浮按钮点击
+     * 悬浮按钮点击 Hover button click
      */
     public void voiceInput(View view) {
         //请求权限
@@ -151,12 +152,12 @@ public class VoiceInputActivity extends MvpActivity<VoiceContract.VoicePresenter
     @SuppressLint("CheckResult")
     private void requestPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            //Android6.0及以上版本
+            //Android6.0及以上版本 Android6. 0 and above
             rxPermissions.request(Manifest.permission.RECORD_AUDIO).subscribe(granted -> {
-                if (granted) {//权限通过
-                    //开始语音监听
+                if (granted) {//权限通过 Permission passed
+                    //开始语音监听 Start voice monitoring
                     startVoiceListener();
-                } else {//权限未通过
+                } else {//权限未通过 Permission failed
                     showMsg("权限未通过，你不能使用该功能");
                 }
             });
