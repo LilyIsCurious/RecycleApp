e19ebbb0d75064d4b70ae39ca25204e03b077394
lcw <91087872+LilyIsCurious@users.noreply.github.com>
2022-01-30 14:54:26 -0500

    Update TextInputActivity.java

--- app/src/main/java/com/example/myrecycle/ui/TextInputActivity.java
+++ app/src/main/java/com/example/myrecycle/ui/TextInputActivity.java
@@ -27,16 +27,17 @@ import java.util.List;
 
 /**
  * 手动输入物品进行垃圾分类
+ * Manually enter items for garbage classification
  */
 public class TextInputActivity extends MvpActivity<TextContract.TextPresenter> implements TextContract.TextView {
 
 
     private static final String TAG = "TextInputActivity";
-    private EditText etGoods;//输入框
-    private ImageView ivClear;//清空输入框
-    private RecyclerView rvResult;//结果显示列表
-    private List<TrashResponse.NewslistBean> newslistBeanList = new ArrayList<>();//数据列表
-    private SearchGoodsAdapter searchGoodsAdapter;//结果列表适配器
+    private EditText etGoods;//输入框 text box
+    private ImageView ivClear;//清空输入框 clear text box
+    private RecyclerView rvResult;//结果显示列表 show result list
+    private List<TrashResponse.NewslistBean> newslistBeanList = new ArrayList<>();//数据列表 data list
+    private SearchGoodsAdapter searchGoodsAdapter;//结果列表适配器 Result list adapter
     private MaterialToolbar toolbar;//工具栏
 
     @Override
@@ -56,9 +57,10 @@ public class TextInputActivity extends MvpActivity<TextContract.TextPresenter> i
 
     /**
      * 页面初始化
+     * initialize
      */
     private void initView() {
-        //设置状态栏
+        //设置状态栏 Set status bar
         setStatubar(this, R.color.white, true);
         toolbar = findViewById(R.id.toolbar);
         back(toolbar, false);
@@ -66,18 +68,18 @@ public class TextInputActivity extends MvpActivity<TextContract.TextPresenter> i
         etGoods = findViewById(R.id.et_goods);
         ivClear = findViewById(R.id.iv_clear);
         rvResult = findViewById(R.id.rv_result);
-        //配置适配器  设置布局和数据源
+        //配置适配器  设置布局和数据源 Configure adapter settings layout and data source
         searchGoodsAdapter = new SearchGoodsAdapter(R.layout.item_search_rv, newslistBeanList);
-        //设置列表的布局管理器
+        //设置列表的布局管理器 Set the layout manager for the list
         rvResult.setLayoutManager(new LinearLayoutManager(this));
-        //列表item点击事件
+        //列表item点击事件 List item click event
         searchGoodsAdapter.setOnItemChildClickListener((adapter, view, position) -> {
             showMsg("点击了" + newslistBeanList.get(position).getName());
         });
-        //设置列表适配器
+        //设置列表适配器 Set list adapter
         rvResult.setAdapter(searchGoodsAdapter);
 
-        //设置输入监听
+        //设置输入监听 Set input listening
         etGoods.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -98,7 +100,7 @@ public class TextInputActivity extends MvpActivity<TextContract.TextPresenter> i
 
             }
         });
-        //设置动作监听
+        //设置动作监听 Set action monitoring
         etGoods.setOnEditorActionListener((v, actionId, event) -> {
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
 
@@ -106,18 +108,18 @@ public class TextInputActivity extends MvpActivity<TextContract.TextPresenter> i
                 if (word.isEmpty()) {
                     showMsg("请输入物品名");
                 } else {
-                    //显示加载弹窗
+                    //显示加载弹窗 Display loading pop-up
                     showLoadingDialog();
-                    //控制输入法
+                    //控制输入法 Control input method
                     controlInputMethod();
-                    //请求接口
+                    //请求接口 Request interface
                     mPresenter.searchGoods(word);
                 }
                 return true;
             }
             return false;
         });
-        //清空输入框内容
+        //清空输入框内容 Clear the contents of the input box
         ivClear.setOnClickListener(v -> {
             controlInputMethod();
             etGoods.setText("");
@@ -127,6 +129,7 @@ public class TextInputActivity extends MvpActivity<TextContract.TextPresenter> i
     /**
      * 控制输入法
      * 当输入法打开时关闭，关闭时弹出
+     * Close when the input method is turned on and pop up when it is turned off
      */
     private void controlInputMethod() {
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -140,20 +143,20 @@ public class TextInputActivity extends MvpActivity<TextContract.TextPresenter> i
      */
     @Override
     public void getSearchResponse(TrashResponse response) {
-        //隐藏加载弹窗
+        //隐藏加载弹窗 Hide load pop-up
         hideLoadingDialog();
         if (response.getCode() == Constant.SUCCESS_CODE) {
-            //请求成功  进行数据的渲染
+            //请求成功  进行数据的渲染 Request to render data successfully
             if (response.getNewslist() != null && response.getNewslist().size() > 0) {
                 newslistBeanList.clear();
                 newslistBeanList.addAll(response.getNewslist());
-                //刷新适配器
+                //刷新适配器 Refresh adapter
                 searchGoodsAdapter.notifyDataSetChanged();
             } else {
                 showMsg("触及到了知识盲区");
             }
         } else {
-            //显示请求接口失败的原因
+            //显示请求接口失败的原因 Displays the reason why the request interface failed
             showMsg(response.getMsg());
         }
     }
